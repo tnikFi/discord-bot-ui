160d082209bc5cdeab332a9d2076d122fc76a2f8
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-11-17 12:49:34 +0200

    Added page content

--- src/components/ContentWrapper.tsx
+++ src/components/ContentWrapper.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Box, Stack } from '@mui/material';
 import Sidebar from './Sidebar';
+import Page from '../pages/dashboard/Page';
 
 const ContentWrapper = () => {
     return (
-        <Stack sx={{ flexDirection: { sx: 'column', md: 'row', sd: 'row' }, flexGrow: '1 !important' }}>
+        <Stack sx={{ flexFlow: 'row nowrap', flexGrow: '1 !important' }}>
             <Box sx={{ height: 'calc(90vh - 4rem)'}}>
                 <Sidebar />
             </Box>
+            <Page />
         </Stack>
     );
 };
---
+++ src/pages/dashboard/Page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Page.module.css';
+
+const Page = () => {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    );
+};
+
+export default Page;
